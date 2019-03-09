c41ddab74a509bca929e8634cbb7e33a4435c32a
ccdle12 <chris.coverdale24@gmail.com>
2019-03-09 11:33:10 +0000

    src/main+output: adding error print and write to output

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     println!("{:?}", args);
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing the arguments: {}", err);
+        eprintln!("Problem parsing the arguments: {}", err);
         process::exit(1);
     });
     println!("Searching for {}", config.query);
@@ -18,7 +18,7 @@ fn main() {
     // We are using this way to catch the error as opposed to unwrap_or_else because run does not
     // return any values besides an error.
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1)
     }
 }
